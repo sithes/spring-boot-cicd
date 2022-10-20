5ca3d1b5b0add629284fa8fb1d4873f920ceb3d7
sithes <sithes.nmc@gmail.com>
2022-10-20 18:26:25 +0800

    Added new controller and test case for testing purpose.

--- src/test/java/com/sithes/springbootcicd/SpringBootCicdApplicationTests.java
+++ src/test/java/com/sithes/springbootcicd/SpringBootCicdApplicationTests.java
@@ -1,13 +1,22 @@
 package com.sithes.springbootcicd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringBootCicdApplicationTests {
 
+	@Autowired
+	HelloController helloController;
 	@Test
-	void contextLoads() {
+	void indexTest() {
+		Assert.hasText(helloController.index(),"You are currently in Index Home Page");
+	}
+	@Test
+	void helloTest() {
+		Assert.hasText(helloController.hello("Sithes"),"You are currently in Hello Page with [Sithes]");
 	}
 
 }
